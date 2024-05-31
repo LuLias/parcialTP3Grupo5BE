63306a068ea1725fea0789af361264e92981009b
LuLias <lucia.liascovich@gmail.com>
2024-05-30 22:41:02 -0300

    recycler results pre retrofit + navigation search to results

---
+++ app/src/main/java/com/example/parcialtp3grupo5be/entities/SearchResultsModel.kt
@@ -0,0 +1,16 @@
+package com.example.parcialtp3grupo5be.entities
+
+class SearchResultsModel (
+
+    val logo_airline: String,
+    val airline: String,
+    val duration: Int,
+    val departure_airport_id: String,
+    val arrival_airport_id: String,
+    val departure_airport_name: String,
+    val arrival_airport_name: String,
+    val travel_class: String,
+    val price: Int
+
+) {
+}
--- app/src/main/java/com/example/parcialtp3grupo5be/fragments/SearchResultsFragment.kt
+++ app/src/main/java/com/example/parcialtp3grupo5be/fragments/SearchResultsFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.parcialtp3grupo5be.R
+import com.example.parcialtp3grupo5be.adapters.SearchResultsAdapter
+import com.example.parcialtp3grupo5be.providers.SearchResultsProvider
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,16 +18,19 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
+ * Use the [SearchResultsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SearchFragment : Fragment() {
+class SearchResultsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    lateinit var view1 : View
-    lateinit var btnToResults : Button
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initSearchResultsRV(view)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,18 +44,18 @@ class SearchFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        view1 = inflater.inflate(R.layout.fragment_search, container, false)
-        btnToResults = view1.findViewById(R.id.btnToSearchResults)
-
-        btnToResults.setOnClickListener {
-            val action = SearchFragmentDirections.actionSearchFragmentToSearchResultsFragment()
-            view1.findNavController().navigate(action)
-        }
-
-
-        return view1
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_results, container, false)
     }
+
+    private fun initSearchResultsRV(view : View){
+        val recyclerView = view.findViewById<RecyclerView>(R.id.searchResultRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager (context, LinearLayoutManager.VERTICAL,false)
+        recyclerView.adapter = SearchResultsAdapter(SearchResultsProvider.SearchResultsList)
+    }
+
+
+
 
     companion object {
         /**
@@ -59,12 +64,12 @@ class SearchFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment search.
+         * @return A new instance of fragment SearchResultsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
+            SearchResultsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
