871247a184ddd6dff336d6fc5ef4231f8a550b36
LuLias <lucia.liascovich@gmail.com>
2024-06-02 03:22:04 -0300

    nav profile to settings + layout settings

--- app/src/main/java/com/example/parcialtp3grupo5be/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/parcialtp3grupo5be/fragments/SettingsFragment.kt
@@ -1,16 +1,14 @@
 package com.example.parcialtp3grupo5be.fragments
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.navigation.fragment.findNavController
 import com.example.parcialtp3grupo5be.R
+import com.example.parcialtp3grupo5be.R.id.txtArgumentos
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,10 +17,15 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
+ * Use the [SettingsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ProfileFragment : Fragment() {
+class SettingsFragment : Fragment() {
+
+    lateinit var settingsFragmentView : View
+    lateinit var recibeArgumentos : TextView
+
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -40,35 +43,23 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        settingsFragmentView = inflater.inflate(R.layout.fragment_settings, container, false)
+        recibeArgumentos = settingsFragmentView.findViewById(txtArgumentos)   //el imageview
 
-        val btnBack = view.findViewById<ImageButton>(R.id.btnBackProfile)
-        val toSettings = view.findViewById<ImageView>(R.id.imgSettingsProfile)
-        val toSettingsFromText = view.findViewById<TextView>(R.id.txtSettingsProfile)
-        val toSettingsFromPayment = view.findViewById<ImageView>(R.id.imgPaymentProfile) //SACAR!
-      //  val avatar = view.findViewById<TextView>(R.id.imgAvatarProfile)
-        val avatar= "Avatar"
 
-        btnBack.setOnClickListener(){
-            val action = R.id.action_profileFragment_to_exploreFragment
-            findNavController().navigate(action)
-        }
-
-        toSettings.setOnClickListener(){
-            findNavController().navigate(R.id.action_profileFragment_to_settingsFragment)
-        }
-        toSettingsFromPayment.setOnClickListener(){
-            val action = ProfileFragmentDirections.actionProfileFragmentToSettingsFragment(avatar)
-            findNavController().navigate(action)
-        }
-
-        toSettingsFromText.setOnClickListener(){
-            val action = R.id.action_profileFragment_to_exploreFragment
-            findNavController().navigate(action)
-        }
-
-        return view
+        return settingsFragmentView
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        val parametro = SettingsFragmentArgs.fromBundle(requireArguments()).unAvatar
+        recibeArgumentos.text = parametro
+
+
+
+    }
+
 
     companion object {
         /**
@@ -77,12 +68,12 @@ class ProfileFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
+         * @return A new instance of fragment SettingsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
+            SettingsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
